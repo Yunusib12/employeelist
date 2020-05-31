f2187bcd4fa27ecece51f67d9ace71b73c67a7e9
Ibrahim Yunus <younis.ib@gmail.com>
2020-05-30 23:43:04 -0400

    updated port number

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const rp = require('request-promise');
 const app = express();
-const PORT = process.env.PORT | 3000;
+const PORT = process.env.PORT | 3001;
 
 app.get('/', (req, res) => {
 
